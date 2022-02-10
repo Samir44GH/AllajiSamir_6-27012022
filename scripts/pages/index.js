22b508ccc0a870746aa0a3f1db3f1f66578df2e5
ALLAJI <samir27021986@hotmail.fr>
2022-02-10 15:13:10 +0100

    Mep photographer.jsonDEBUT

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,61 +1,6 @@
 async function getPhotographers() {
   // Penser à remplacer par les données récupérées dans le json
-  const photographers = [
-    {
-      name: "Mimi Keel",
-      id: 243,
-      city: "London",
-      country: "UK",
-      tagline: "Voir le beau dans le quotidien",
-      price: 400,
-      portrait: "MK.jpg",
-    },
-    {
-      name: "Ellie-Rose Wilkens",
-      id: 930,
-      city: "Paris",
-      country: "France",
-      tagline: "Capturer des compositions complexes",
-      price: 250,
-      portrait: "ER.jpg",
-    },
-    {
-      name: "Tracy Galindo",
-      id: 82,
-      city: "Montréal",
-      country: "Canada",
-      tagline: "Photographe freelance",
-      price: 500,
-      portrait: "TG.jpg",
-    },
-    {
-      name: "Nabeel Bradford",
-      id: 527,
-      city: "Mexico city",
-      country: "Mexico",
-      tagline: "Toujours aller de l'avant",
-      price: 350,
-      portrait: "NB.jpg",
-    },
-    {
-      name: "Rhode Dubois",
-      id: 925,
-      city: "Barcelona",
-      country: "Spain",
-      tagline: "Je crée des souvenirs",
-      price: 275,
-      portrait: "RD.jpg",
-    },
-    {
-      name: "Marcel Nikolic",
-      id: 195,
-      city: "Berlin",
-      country: "Germany",
-      tagline: "Toujours à la recherche de LA photo",
-      price: 300,
-      portrait: "Marcel.jpg",
-    },
-  ];
+  const photographers = [];
   // et bien retourner le tableau photographers seulement une fois
   return {
     photographers: [...photographers],
@@ -75,11 +20,11 @@ async function displayData(photographers) {
 async function init() {
   // Récupère les datas des photographes
   const { photographers } = await getPhotographers();
-  displayData(photographers);
+  //displayData(photographers);
 }
 
 init();
 
 fetch("data/photographers.json")
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => displayData(data.photographers));
